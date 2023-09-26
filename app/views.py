979de0f5a32ecb2835d8e009be7b8ac330563ebb
TienBruyne <nguyentrongtiennl1@gmail.com>
2023-09-26 14:59:43 +0700

    update

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    context = {}
+    return render(request,'app/home.html',context)
+def login(request):
+    context = {}
+    return render(request,'app/login.html',context)
+def signup(request):
+    context = {}
+    return render(request,'app/signup.html',context)
