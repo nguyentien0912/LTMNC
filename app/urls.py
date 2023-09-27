625cad63a064935777c5fbf136cd9214c8a2731a
TienBruyne <nguyentrongtiennl1@gmail.com>
2023-09-27 22:48:33 +0700

    update

--- app/urls.py
+++ app/urls.py
@@ -19,8 +19,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home,name="home"),
-    path('login/',views.login, name="login"),
+    path('home/', views.home,name="home"),
+    path('',views.login, name="login"),
     path('signup/',views.signup, name="signup"),
    
 ]
